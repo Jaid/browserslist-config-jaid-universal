e83a4fadaa9d854551b29d6915397d7ba77812c8
Jaid <jaid.jsx@gmail.com>
2019-07-08 10:36:41 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 /** @module browserslist-config-jaid-universal */
 
+import browserslistConfigJaidNode from "browserslist-config-jaid-node"
+import browserslistConfigJaidWeb from "browserslist-config-jaid-web"
+
+const mergedQuery = [
+  ...browserslistConfigJaidNode,
+  ...browserslistConfigJaidWeb,
+].filter(version => version !== "not dead")
+
 /**
  * Exports a Browserslist query
  * @type {string[]}
  */
 export default [
-  "node >= 10",
+  ...mergedQuery,
   "not dead",
 ]
