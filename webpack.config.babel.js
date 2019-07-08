7b68319d8bebe3b45730de1c964fec022be52d55
Jaid <jaid.jsx@gmail.com>
2019-07-08 14:03:21 +0200

    manage: Changed Webpack `libraryTarget` to `commonjs2`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,4 +3,8 @@ import configure from "webpack-config-jaid"
 export default configure({
   documentation: true,
   publishimo: {fetchGithub: true},
+  nodeExternals: false,
+  output: {
+    libraryTarget: "commonjs2",
+  },
 })
