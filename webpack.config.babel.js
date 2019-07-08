203335981e7d015cc2412bc78cc06141511adcb7
Jaid <jaid.jsx@gmail.com>
2019-07-08 14:05:26 +0200

    Removed unneeded `webpack-config-jaid` option
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,6 @@ import configure from "webpack-config-jaid"
 export default configure({
   documentation: true,
   publishimo: {fetchGithub: true},
-  nodeExternals: false,
   output: {
     libraryTarget: "commonjs2",
   },
