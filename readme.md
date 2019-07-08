e76966521828b13e1925944a4c0c799eee510f38
Jaid <jaid.jsx@gmail.com>
2019-07-08 10:05:16 +0200

    Initialized based on https://github.com/Jaid/browserslist-config-jaid-node

---
+++ readme.md
@@ -0,0 +1,3 @@
+# browserslist-config-jaid-universal
+## Under construction
+Not much to see right now. Come back lator! :crocodile:
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/** @module browserslist-config-jaid-universal */
+
+/**
+ * Exports a Browserslist query
+ * @type {string[]}
+ */
+export default [
+  "node >= 10",
+  "not dead",
+]
